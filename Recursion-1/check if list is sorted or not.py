749930e451b8eb0898b2faf303558daa2b01ea0d
Rithika2000 <57192807+Rithika2000@users.noreply.github.com>
2022-02-12 20:17:30 +0530

    Create check if list is sorted or not.py

---
+++ Recursion-1/check if list is sorted or not.py
@@ -0,0 +1,17 @@
+def isSorted(l):
+    list_ = len(l)
+    if list_ == 0 or list_ == 1 :
+        return True
+    
+    if l[0] > l[1]:
+        return False
+    
+    smallerList = l[1:]
+    smallerListOutput = isSorted(smallerList)
+    if smallerListOutput:
+        return True
+    else:
+        return False
+
+l = [1,2,93,4,5]
+isSorted(l)
