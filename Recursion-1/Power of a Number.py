a1c1fec9096c45a461644c5e536b1ad4325fdac7
Rithika2000 <57192807+Rithika2000@users.noreply.github.com>
2022-02-12 20:05:58 +0530

    Update Power of a Number.py

--- Recursion-1/Power of a Number.py
+++ Recursion-1/Power of a Number.py
@@ -1,3 +1,10 @@
+"""
+
+Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
+Note : For this question, you can assume that 0 raised to the power of 0 is 1
+
+"""
+
 def exp(a,b):
     if b==1:
         return a
